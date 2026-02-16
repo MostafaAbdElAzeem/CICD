9b60e51a0a2323afd1ea9ffe3866f50e2ca274f8
sasa <blackwind99m16@gmail.com>
2026-02-16 13:16:36 +0200

    ss

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echo hello
+
+# Set cowfile path
 COWPATH=/usr/share/cowsay/cows
 
+# Print camel to dragon.txt
 /usr/games/cowsay -f $COWPATH/camel.cow "Run for cover, I am a Dragon... RAWR" >> dragon.txt
 
+# Show content
 cat dragon.txt
+
+# List files
 ls
